f36abbc0af64bed794e13d0a0477cfb2491940fc
CT-Hung <poilkj09862@gmail.com>
2018-07-14 15:57:15 +0800

    build

---
+++ build/src/pCommunicationAngle_CTHung/CMakeFiles/pCommunicationAngle_CTHung.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pCommunicationAngle_CTHung.dir/CommunicationAngle_CTHung.cpp.o"
+  "CMakeFiles/pCommunicationAngle_CTHung.dir/CommunicationAngle_CTHung_Info.cpp.o"
+  "CMakeFiles/pCommunicationAngle_CTHung.dir/main.cpp.o"
+  "CMakeFiles/pCommunicationAngle_CTHung.dir/soundTrack.cpp.o"
+  "../../../bin/pCommunicationAngle_CTHung.pdb"
+  "../../../bin/pCommunicationAngle_CTHung"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pCommunicationAngle_CTHung.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/pPrimeFactorTester/CMakeFiles/pPrimeFactorTester.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pPrimeFactorTester.dir/PrimeFactorTester.cpp.o"
+  "CMakeFiles/pPrimeFactorTester.dir/PrimeFactorTester_Info.cpp.o"
+  "CMakeFiles/pPrimeFactorTester.dir/main.cpp.o"
+  "CMakeFiles/pPrimeFactorTester.dir/getVal.cpp.o"
+  "CMakeFiles/pPrimeFactorTester.dir/primeTest.cpp.o"
+  "../../../bin/pPrimeFactorTester.pdb"
+  "../../../bin/pPrimeFactorTester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pPrimeFactorTester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
